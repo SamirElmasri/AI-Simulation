c3a7420e60b01eae4e24e10994f587c737810e5a
SamirElmasri <74344081+SamirElmasri@users.noreply.github.com>
2021-03-22 23:28:42 +0300

    Update Beam_Fea.py

--- Beam_Fea.py
+++ Beam_Fea.py
@@ -8,41 +8,71 @@ Created on Sun Mar 21 20:49:51 2021
 
 import numpy as np
 
-def mesh(L,Element_number,F)
+def mesh(L,Element_number,F):
     
     if F[1] == 'distributed':
 #add an  if statement if the remaineder of L/element_number is not 0
-    nodes = np.arange(0,L+1,L/Element_number)
+        nodes = np.arange(0,L+1,L/Element_number)
     
     return nodes
 
-
+#create the stifnees matrix
 class stifness_m:
     def __init__(self, nodes, Element, L):
-      Matrix = np.empty((len(nodes)*2,len(nodes)*2))
-      Matrix[Element][Element] = 12
-      Matrix[Element][Element+1] = 6*L
-      Matrix[Element][Element+2] = -12
-      Matrix[Element][Element+3] = 6*L
-      Matrix[Element+1][Element] = 6*L
-      Matrix[Element+1][Element+1] = 4*L**2
-      Matrix[Element+1][Element+2] = -6*L
-      Matrix[Element+1][Element+3] = 2*L**2
-      Matrix[Element+2][Element] = -12
-      Matrix[Element+2][Element+1] = -6*L
-      Matrix[Element+2][Element+2] = 12
-      Matrix[Element+2][Element+3] = -6*L
-      Matrix[Element+3][Element] = 6*L
-      Matrix[Element+3][Element+1] = 2*L**2
-      Matrix[Element+3][Element+2] = -6*L
-      Matrix[Element+3][Element+3] = 4*L**2
+      Matrix = np.zeros((len(nodes)*2,len(nodes)*2))
+      Matrix[Element*2][Element*2] = 12
+      Matrix[Element*2][Element*2+1] = 6*L
+      Matrix[Element*2][Element*2+2] = -12
+      Matrix[Element*2][Element*2+3] = 6*L
+      Matrix[Element*2+1][Element*2] = 6*L
+      Matrix[Element*2+1][Element*2+1] = 4*L**2
+      Matrix[Element*2+1][Element*2+2] = -6*L
+      Matrix[Element*2+1][Element*2+3] = 2*L**2
+      Matrix[Element*2+2][Element*2] = -12
+      Matrix[Element*2+2][Element*2+1] = -6*L
+      Matrix[Element*2+2][Element*2+2] = 12
+      Matrix[Element*2+2][Element*2+3] = -6*L
+      Matrix[Element*2+3][Element*2] = 6*L
+      Matrix[Element*2+3][Element*2+1] = 2*L**2
+      Matrix[Element*2+3][Element*2+2] = -6*L
+      Matrix[Element*2+3][Element*2+3] = 4*L**2
+      
+      self.Matrix = Matrix
                  
 def stifness_matrix(nodes, Element_number,L):
     
     indv_stifness_matrix = []
-    for Element in range(len(Element_number)):
+    global_matrix = np.zeros((len(nodes)*2,len(nodes)*2))
+    for Element in range(Element_number):
+        class_element = stifness_m(nodes,Element,L)
+        indv_stifness_matrix.append(class_element)
+        global_matrix = global_matrix + class_element.Matrix
         
+    return indv_stifness_matrix , global_matrix
         
+def boundary_conditions(nodes,Element_number, L , F):
+    
+    if F[1] == 'distributed':
+        if F[2][1] == L:
+            F_nodes = np.array([])
+            F_at_node = L/len(nodes)
+            for i in range(len(nodes)):
+                moment = 'M' + str(i)
+                F_nodes = np.append(F_nodes, [i , F_at_node , moment])
+    
+    Deformation_nodes = np.array([])
+    
+    for i in nodes:
+        d = 'd' + str(i)
+        r = 'r' + str(i)
+        Deformation_nodes = np.append(Deformation_nodes , [i , d ,r])
+                
+    for i in B:
+       if i[0] in nodes:
+           if 
+        
+    
+    
 
 def main():
     
@@ -61,5 +91,6 @@ def main():
     Element_number = 5
     
     nodes = mesh(L, Element_number,F)
+    indv_stifness_matrix , global_matrix = stifness_matrix(nodes, Element_number , L)
     
     
